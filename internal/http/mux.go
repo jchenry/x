e3a6d711d1c2dc94c4b1b46a814019d38f88de7b
Colin Henry <jchenry@users.noreply.github.com>
2020-07-13 10:05:34 -0700

    removed internal package

--- internal/http/mux.go
+++
@@ -1,8 +0,0 @@
-package http
-
-import "net/http"
-
-type Mux interface {
-	Handle(pattern string, handler http.Handler)
-	HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request))
-}
