85df14e0c6ff125088c2340165cfd88b70f3431e
Colin Henry <jchenry@users.noreply.github.com>
2020-08-18 17:47:14 -0700

    commented code cleanup

--- encoding/json/json.go
+++ encoding/json/json.go
@@ -12,12 +12,3 @@ func Encoder(w io.Writer, e interface{}) error {
 func Decoder(r io.Reader, e interface{}) error {
 	return json.NewDecoder(r).Decode(e)
 }
-
-// func Decoder(get func() interface{}) func(io.Reader) (interface{}, error) {
-// 	//TODO I dont like the get() function, find a better way of dealing with this
-// 	return func(r io.Reader) (interface{}, error) {
-// 		e := get()
-// 		err := json.NewDecoder(r).Decode(e)
-// 		return e, err
-// 	}
-// }
--- encoding/xml/xml.go
+++ encoding/xml/xml.go
@@ -12,13 +12,3 @@ func Encoder(w io.Writer, e interface{}) error {
 func Decoder(r io.Reader, e interface{}) error {
 	return xml.NewDecoder(r).Decode(e)
 }
-
-// func Decoder(get func() interface{}) func(io.Reader) (interface{}, error) {
-// 	//TODO I dont like the get() function, find a better way of dealing with this
-// 	return func(r io.Reader) (interface{}, error) {
-// 		e := get()
-// 		err := xml.NewDecoder(r).Decode(e)
-// 		return e, err
-// 	}
-// }
-//
