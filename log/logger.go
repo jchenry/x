ed136ca83e2bd68cc16e1564b31df90a231ecff5
Colin Henry <jchenry@users.noreply.github.com>
2020-08-22 15:47:37 -0700

    new logging package, just the essentials

---
+++ log/logger.go
@@ -0,0 +1,15 @@
+package log
+
+// Logger is a loggin interface with only the essentials that a function that needs to log should care about. Compatible with  standard Go logger.
+type Logger interface {
+	Print(v ...interface{})
+	Printf(format string, v ...interface{})
+	Println(v ...interface{})
+}
+
+// None provides a logger that doesnt log anything
+type None struct{}
+
+func (n None) Print(v ...interface{})                 {}
+func (n None) Printf(format string, v ...interface{}) {}
+func (n None) Println(v ...interface{})               {}
