1ec6f3c5c3de4088499235bf045911aca6e19af8
Colin Henry <jchenry@users.noreply.github.com>
2023-04-04 23:19:05 -0700

    updated doc

--- database/doc.go
+++ database/doc.go
@@ -7,7 +7,7 @@ Example usage:
 	ctx, _ := context.WithCancel(context.Background())
 	dba = &db.Actor{
 		DB:         s.db,
-		ActionChan: make(chan db.Func),
+		ActionChan: make(chan database.Func),
 	}
 
 	go dba.Run(ctx)
