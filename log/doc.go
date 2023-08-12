4665f2ccf9e94a69fdfac69dbf6b4dc17bb2685f
Colin Henry <jchenry@users.noreply.github.com>
2023-08-11 21:59:16 -0700

    deprecating packages

---
+++ log/doc.go
@@ -0,0 +1,3 @@
+package log
+
+// Deprecated: use log/slog
---
+++ rest/doc.go
@@ -0,0 +1,5 @@
+package rest
+
+// Deprecated: I wrote this code when i was foolish enough to think you should use such things.
+// I know better now. http.Handlers are properly structured helper functions are enough.
+// I will remove this at some point //TODO
